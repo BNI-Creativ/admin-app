c1f9ecf0e9feaa83177f7994e9c88e9e43a9859e
Alexandru Ardelean <alex@shruggie.ro>
2026-02-08 10:13:59 +0200

    add deployment changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -100,7 +100,7 @@ function AppRoutes() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/bni-creativ-prezenta">
       <AuthProvider>
         <OfflineProvider>
           <AppRoutes />
